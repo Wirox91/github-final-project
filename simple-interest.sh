4401f7984b7c5ab083a45c5aac31eea6fbe8ac9e
Wirox91 <121252785+Wirox91@users.noreply.github.com>
2023-04-14 11:24:38 -0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # Wirox.91
+   # Wirox91
    # Input:
    # p, principal amount
    # t, time period in years
