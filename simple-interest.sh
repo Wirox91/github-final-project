e6e214c3497ebf9fc056508f7ab3ceabe819ced7
Wirox91 <121252785+Wirox91@users.noreply.github.com>
2025-01-27 10:25:46 -0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -11,12 +11,12 @@
    # r, annual rate of interest
    # Output:
    # simple interest = p*t*r
-   echo "Enter the principal:"
-   read $1
-   echo "Enter rate of interest per year:"
-   read $2
-   echo "Enter time period in years:"
-   read $3
+   #echo "Enter the principal:"
+   #read $1
+   #echo "Enter rate of interest per year:"
+   #read $2
+   #echo "Enter time period in years:"
+   #read $3
    s=`expr $1 \* $3 \* $2 / 100`
    echo "The simple interest is: "
    echo $s
